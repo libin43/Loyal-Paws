1cd22313283307bcb83d42b7d93f9c91a13c507b
Libin Biji <libinbiji43@gmail.com>
2022-12-07 18:07:02 +0530

    Mongo Atlas Created

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mongoClient = require('mongodb').MongoClient
+let uri = 'mongodb+srv://dbLibin:dbLibin@cluster0.pmud7ij.mongodb.net/test'
 const state={
     db:null
 }
 
 module.exports.connect = function(done){
-    const url = 'mongodb://localhost:27017'
+    const url = uri
     const dbname ='PawsIndia'
 
 
